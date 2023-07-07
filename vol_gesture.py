1c9ed19ddb2e2811114a40726ceaa53932b43c3f
Avishek <avishekmukherje007@gmail.com>
2023-07-07 22:14:42 +0530

    Initial Commit

---
+++ vol_gesture.py
@@ -0,0 +1,55 @@
+import cv2
+import mediapipe as mp
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+def set_volume(volume):
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume_object = cast(interface, POINTER(IAudioEndpointVolume))
+    volume_object.SetMasterVolumeLevelScalar(volume, None)
+
+def main():
+    cap = cv2.VideoCapture(0)
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    mp_drawing = mp.solutions.drawing_utils
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.flip(frame, 1)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+
+                thumb_x, thumb_y = int(thumb_tip.x * frame.shape[1]), int(thumb_tip.y * frame.shape[0])
+                index_x, index_y = int(index_tip.x * frame.shape[1]), int(index_tip.y * frame.shape[0])
+
+                cv2.circle(frame, (thumb_x, thumb_y), 10, (0, 255, 0), -1)
+                cv2.circle(frame, (index_x, index_y), 10, (0, 255, 0), -1)
+
+                if thumb_x < index_x:
+                    # Increase volume
+                    set_volume(1.0)
+                else:
+                    # Decrease volume
+                    set_volume(0.0)
+
+        cv2.imshow('Gesture Control', frame)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
